a34def46b03599fcb25c111aa48acab22ab9724a
Gideon Isa <innocentgideon10@gmail.com>
2023-05-06 19:19:46 +0100

    adding many to one mapping

--- src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -1,13 +1,20 @@
 package com.ltp.gradesubmission;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.repository.StudentRepository;
 
 @SpringBootApplication
 public class GradeSubmissionApplication implements CommandLineRunner{
+
+	@Autowired
+	StudentRepository studentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeSubmissionApplication.class, args);
@@ -17,7 +24,17 @@ public class GradeSubmissionApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'run'");
+		Student[] students = new Student[] {
+			new Student(1L, "Harry Potter", LocalDate.parse("1980-07-31")),
+			new Student(2L, "Ron Weasley", LocalDate.parse("1980-03-01")),
+			new Student(3L, "Hermione Granger", LocalDate.parse("1979-09-19")),
+			new Student(4L, "Neville", LocalDate.parse("1980-07-30"))
+		};
+
+		for (int i = 0; i < students.length; i++) {
+			studentRepository.save(students[i]);
+		}
+		//throw new UnsupportedOperationException("Unimplemented method 'run'");
 	}
 
 }
--- src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
+++ src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
@@ -2,12 +2,22 @@ package com.ltp.gradesubmission.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ltp.gradesubmission.entity.Grade;
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.repository.GradeRepository;
+import com.ltp.gradesubmission.repository.StudentRepository;
 
 @Service
 public class GradeServiceImpl implements GradeService {
+
+    @Autowired
+    GradeRepository gradeRepository;
+
+    @Autowired
+    StudentRepository studentRepository;
     
     @Override
     public Grade getGrade(Long studentId, Long courseId) {
@@ -16,9 +26,12 @@ public class GradeServiceImpl implements GradeService {
 
     @Override
     public Grade saveGrade(Grade grade, Long studentId, Long courseId) {
-        return null;
+        // need to assign the grade to a student before saving
+        Student student = studentRepository.findById(studentId).get();
+        grade.setStudent(student);
+        return gradeRepository.save(grade);
     }
-
+ 
     @Override
     public Grade updateGrade(String score, Long studentId, Long courseId) {
         return null;
